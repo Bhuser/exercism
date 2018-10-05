24922eaf8086a8f8df58e94e7aa49cc830062139
Brenna Huser <bhuser@multiservice.com>
2018-10-05 08:30:40 -0500

    Scrabble Alternate solution

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,15 +1,15 @@
 class Scrabble
 
-  MAPPING = {
-      'a' => 1, 'e' => 1, 'i' => 1, 'o' => 1, 'u' => 1,
-      'l' => 1, 'n' => 1, 'r' => 1, 's' => 1, 't' => 1,
-      'd' => 2, 'g' => 2,
-      'b' => 3, 'c' => 3, 'm' => 3, 'p' => 3,
-      'f' => 4, 'h' => 4, 'v' => 4, 'w' => 4, 'y' => 4,
-      'k' => 5,
-      'j' => 8, 'x' => 8,
-      'q' => 10,'z' => 10
-  }
+  # MAPPING = {
+  #     'a' => 1, 'e' => 1, 'i' => 1, 'o' => 1, 'u' => 1,
+  #     'l' => 1, 'n' => 1, 'r' => 1, 's' => 1, 't' => 1,
+  #     'd' => 2, 'g' => 2,
+  #     'b' => 3, 'c' => 3, 'm' => 3, 'p' => 3,
+  #     'f' => 4, 'h' => 4, 'v' => 4, 'w' => 4, 'y' => 4,
+  #     'k' => 5,
+  #     'j' => 8, 'x' => 8,
+  #     'q' => 10,'z' => 10
+  # }
 
   LETTER_VALUES = {
       %w(a e i o u l n r s t) => 1,
@@ -31,7 +31,7 @@ class Scrabble
 
   def score
     return 0 if @word =~ /\A\s*\Z/
-    @word.chars.map{ |char| MAPPING[char.downcase] }.sum
+    @word.chars.map{ |char| letter_values[char.downcase] }.sum
   end
 
   def letter_values
